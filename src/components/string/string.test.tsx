7b56d657a80180141461a37876bb18fe166a70dd
unknown <iuliya21@mail.ru>
2023-08-02 16:44:59 +0300

    start test stringComponent

--- src/components/string/string.test.tsx
+++ src/components/string/string.test.tsx
@@ -1,7 +1,20 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { StringComponent } from './string';
 
-test("Корректное разворот строки с четным количеством символов", async () => {
-  render(<StringComponent />);
-  const input = screen.getByRole("textbox");
+describe("Тестирование алгоритма разворота строки StringComponent", () => {
+  test("Корректное разворот строки с нечетным количеством символов", async () => {
+    render(<StringComponent />);
+    const input = screen.getByRole("textbox");
+    const button = screen.getByRole("button", { name: "Развернуть" });
+  
+    fireEvent.change(input, { target: { value: "hello" } });
+    fireEvent.click(button);
+    
+    await screen.findByText("o");
+    await screen.findByText("l");
+    await screen.findByText("l");
+    await screen.findByText("e");
+    await screen.findByText("h");
+  });
 })
+
--- src/components/ui/circle/circle.test.jsx
+++ src/components/ui/circle/circle.test.jsx
@@ -1,57 +1,61 @@
 import { render } from '@testing-library/react';
 import { Circle } from './circle';
+import { ElementStates } from '../../../types/element-states';
 
-test("Отрисовка компонента Circle без символа", () => {
-  const { asFragment } = render(<Circle />);
-  expect(asFragment()).toMatchSnapshot();
+describe('Тестирование компонента Circle', () => {
+  test("Отрисовка компонента Circle без символа", () => {
+    const { asFragment } = render(<Circle />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с символом", () => {
+    const { asFragment } = render(<Circle letter="Символ" />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с пропсом head", () => {
+    const { asFragment } = render(<Circle head="head" />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с react-элементом в пропсе head", () => {
+    const { asFragment } = render(<Circle head={<div>React Element</div>} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с  пропсом tail", () => {
+    const { asFragment } = render(<Circle tail="tail" />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с react-элементом в пропсе tail", () => {
+    const { asFragment } = render(<Circle tail={<div>React Element</div>} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с index", () => {
+    const { asFragment } = render(<Circle index={1} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с пропсом isSmall ===  true", () => {
+    const { asFragment } = render(<Circle isSmall={true} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с состоянием ElementStates.Default", () => {
+    const { asFragment } = render(<Circle state={ElementStates.Default} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с состоянием ElementStates.Changing", () => {
+    const { asFragment } = render(<Circle state={ElementStates.Changing} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
+  test("Отрисовка компонента Circle с состоянием ElementStates.Modifield", () => {
+    const { asFragment } = render(<Circle state={ElementStates.Modified} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
 
-test("Отрисовка компонента Circle с символом", () => {
-  const { asFragment } = render(<Circle letter="Символ" />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test("Отрисовка компонента Circle с пропсом head", () => {
-  const { asFragment } = render(<Circle head="head" />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test("Отрисовка компонента Circle с react-элементом в пропсе head", () => {
-  const { asFragment } = render(<Circle head={<div>React Element</div>} />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test("Отрисовка компонента Circle с  пропсом tail", () => {
-  const { asFragment } = render(<Circle tail="tail" />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test("Отрисовка компонента Circle с react-элементом в пропсе tail", () => {
-  const { asFragment } = render(<Circle tail={<div>React Element</div>} />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test("Отрисовка компонента Circle с index", () => {
-  const { asFragment } = render(<Circle index={1} />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test("Отрисовка компонента Circle с пропсом isSmall ===  true", () => {
-  const { asFragment } = render(<Circle isSmall />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test("Отрисовка компонента Circle с состоянием ElementStates.Default", () => {
-  const { asFragment } = render(<Circle state={ElementStates.Default} />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test("Отрисовка компонента Circle с состоянием ElementStates.Changing", () => {
-  const { asFragment } = render(<Circle state={ElementStates.Changing} />);
-  expect(asFragment()).toMatchSnapshot();
-});
-
-test("Отрисовка компонента Circle с состоянием ElementStates.Modifield", () => {
-  const { asFragment } = render(<Circle state={ElementStates.Modifield} />);
-  expect(asFragment()).toMatchSnapshot();
-});
